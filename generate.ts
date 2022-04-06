d7106aed042ebac8f2c0282a1be6c0e90b809556
Udit Karode <udit.karode@gmail.com>
2022-04-06 19:02:51 +0530

    doc(generate): add comments

--- generate.ts
+++ generate.ts
@@ -13,6 +13,7 @@ const args = parse(Deno.args, {
   default: { "input-dir": ".", "output-dir": "." },
 }) as unknown as Args;
 
+/// check if `input-dir` and `output-dir` exist
 const dirs = ["input-dir", "output-dir"] as (keyof Args)[];
 dirs.forEach(async (k) => {
   const stat = await Deno.stat(args[k]);
@@ -21,9 +22,20 @@ dirs.forEach(async (k) => {
   }
 });
 
+/// regex to extract enums
 const enumRegex = /enum\s\w+\s{.+?}/gs;
+
+/// object that will contain all enums
+/// Record<filename, array of enum strings>
 const enums: Record<string, string[]> = {};
 
+/**
+ * @description scans @param dir for files ending with
+ * .sol, checks if they contain any enums and if they do,
+ * adds it to `enums`. If they don't displays a warning.
+ * If there are any directories in @param dir, recursively
+ * calls `generate` on them.
+ */
 const generate = async (dir: string) => {
   for await (const file of Deno.readDir(dir)) {
     if (file.isDirectory) {
@@ -36,16 +48,19 @@ const generate = async (dir: string) => {
       continue;
     }
 
+    // read the solidity source
     const content = await Deno.readTextFile(
       `${dir}/${file.name}`,
     );
 
+    // find enum regex matches the source
     const matches = Array.from(
       content.matchAll(enumRegex),
       (x) => `const ${x[0]}`,
     );
 
     if (matches.length != 0) {
+      // if matches exist, add to `enums`
       const filename = file.name.replace(".sol", "");
       if (enums[filename] != undefined) {
         error(
@@ -62,9 +77,18 @@ const generate = async (dir: string) => {
 
 await generate(args["input-dir"]);
 
+/// dir where enum files will be saved
 const enumDir = `${await Deno.realPath(args["output-dir"])}/enums`;
+
+/// make sure `enumDir` exists, or create it if it doesn't
 await fs.ensureDir(enumDir);
 
+/**
+ * writes `enums` to storage. For example, on a value
+ * like { "Lottery": [ "const enum {...}" ] },
+ * it will write ${enumDir}/Lottery.ts containing
+ * "const enum {...}"
+ */
 for (const [k, v] of Object.entries(enums)) {
   const file = `${enumDir}/${k}.ts`;
   await Deno.writeTextFile(
